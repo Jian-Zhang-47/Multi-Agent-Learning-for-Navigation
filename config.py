bae49289bf50482b51bbde5ed1f96ce71e3282a5
Jian Zhang <77740936+NightsCold@users.noreply.github.com>
2024-06-26 22:00:34 +0300

    update

---
+++ config.py
@@ -0,0 +1,5 @@
+L=8             # Grid size 
+P=0.1           # Number of obstacles (as a percentage to grid size)
+N=2             # Number of agents 
+T=5             # Maximum episode length 
+M=2             # Agent FoV size 
